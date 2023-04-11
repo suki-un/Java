3d01cef0deccbd0d9b0690d09e41a0742d530500
lycoriskkk <lycoris5210@gmail.com>
2023-04-11 15:36:30 +0800

    new

---
+++ javaStudent/zuoYe/src/heelll.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class heelll {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = sc.nextInt();
+        }
+        int[] c = new int[n];
+        for (int i = 0; i < n; i++) {
+            c[i] = sc.nextInt();
+        }
+    }
+}
