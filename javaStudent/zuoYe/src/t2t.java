acec2fe2cb9e2a9355ffe2d5717dd42cc09fde97
lycoriskkk <lycoris5210@gmail.com>
2023-04-11 15:41:52 +0800

    lycoris new

---
+++ javaStudent/zuoYe/src/t2t.java
@@ -0,0 +1,8 @@
+public class t2t {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: java t2t <filename>");
+            System.exit(0);
+        }
+    }
+}
