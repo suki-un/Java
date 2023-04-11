cdcce44b8d526b37911c736e5f98894bc71a2604
lycoriskkk <lycoris5210@gmail.com>
2023-04-11 19:15:57 +0800

    lycoris new

--- javaStudent/zuoYe/src/TTTTT.java
+++ javaStudent/zuoYe/src/TTTTT.java
@@ -2,5 +2,6 @@ public class TTTTT {
     public static void main(String[] args) {
         System.out.println("asd");
         System.out.println("sadashdahdjshdjkahdkjhk");
+        System.out.println("测试222");
     }
 }
